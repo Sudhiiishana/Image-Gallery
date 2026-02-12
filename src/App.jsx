b2dd7b79bd868b271e706ac163782aa09bb7c7e8
Sudhiiishana <msudhishana@gmail.com>
2026-02-12 19:01:23 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import Gallery from "./Components/Gallery";
+import Gallery from "./Components/Gallery/Gallery";
+import Home from "./Components/Login/Login";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
